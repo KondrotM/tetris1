36ee11484374883942f92516d46403daaba1974d
KondrotM <kondrot.m@azet.sk>
2018-03-24 19:02:49 +0000

    Oops forgot about game over scene
    Visual revamp to game over scene + a nice scoreboard rather than it all just being one big string

--- Assets/Scripts/DisplayHighscores.cs
+++ Assets/Scripts/DisplayHighscores.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DisplayHighscores : MonoBehaviour {
-	public Text[] highscoreText;
+	public Text[] usernameText;
+	public Text[] scoreText;
+	public Text[] levelText;
+	public Text[] linesText;
+
 	//Reference to the highscore class
 	Highscores highscoreManager;
 
 	void Start () {
 		//Goes through all the texts inside highscoreText that we have,
-		for (int i = 0; i < highscoreText.Length; i++) {
-			highscoreText [i].text = i + 1 + ". Fetching...";
+		for (int i = 0; i < usernameText.Length; i++) {
+			usernameText [i].text = i + 1 + ". Fetching...";
+			scoreText [i].text = i + 1 + ". Fetching...";
+			levelText [i].text = i + 1 + ". Fetching...";
+			linesText [i].text = i + 1 + ". Fetching...";
 		}
 		highscoreManager = GetComponent<Highscores> ();
 
@@ -20,10 +27,18 @@ public class DisplayHighscores : MonoBehaviour {
 	//Appends the fetching text with current highscores,
 	//checks that there are enough highscores to display and if there aren't displays a dot.
 	public void OnHighscoresDownloaded(Highscore[] highscoreList) {
-		for (int i = 0; i < highscoreText.Length; i++) {
-			highscoreText [i].text = i + 1 + ". ";
+		for (int i = 0; i < usernameText.Length; i++) {
+			usernameText [i].text = i + 1 + ". ";
+			scoreText [i].text = "";
+			levelText [i].text = "";
+			linesText [i].text = "";
 			if (highscoreList.Length > i) {
-				highscoreText[i].text += highscoreList[i].username + " - " + highscoreList[i].score + " - " + highscoreList[i].level + " - " + highscoreList[i].lines;
+				usernameText [i].text += highscoreList [i].username; 
+				scoreText [i].text += highscoreList [i].score; 
+				levelText [i].text += highscoreList [i].level; 
+				linesText [i].text += highscoreList [i].lines; 
+
+
 			}
 		}
 	}
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -24,14 +24,14 @@ public class Score : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Sets the text in variables the appropriate things. This is then implemented into objects the unity scene.
-		scoreText.text = "Score: " + Game.Score;
+		scoreText.text = "Score:   " + Game.Score;
 
-		levelText.text = "Level: " + Game.level;
+		levelText.text = "Level:   " + Game.level;
 
-		linesText.text = "Lines: " + Game.linesCleared;
+		linesText.text = "Lines:   " + Game.linesCleared;
 
 		usrText.text = GameOverManager.usrstore;
 
-		goalText.text = "Goal: " + FindObjectOfType<Game> ().levelGoal;
+		goalText.text = "Goal:   " + FindObjectOfType<Game> ().levelGoal;
 	}
 }
